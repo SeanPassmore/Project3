ed6283dda6c3fbff5271cfd5428a5853f4659177
Sean Passmore <jeanghandi@gmail.com>
2019-02-03 21:23:34 -0500

    Finished the notification function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  @counter = 0
+
   # GET /products
   # GET /products.json
   def index
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,14 @@ class StoreController < ApplicationController
 
   def index
     @products = Product.order(:title)
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+      if session[:counter] > 5
+        flash.now[:index] = "You've been here #{session[:counter]} time".pluralize(session[:counter])+". Buy something or leave. This isn't a library."
+        render "index"
+        end
+      end
+    end
   end
-end
