1cce1601ef4ca6356a57c8ea31ed0afba77f700c
Sean Passmore <jeanghandi@gmail.com>
2019-02-05 00:15:22 -0500

    fixed an error with stoe controller

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -8,19 +8,19 @@ class StoreController < ApplicationController
       session[:counter] += 1
       if session[:counter] > 5
         flash.now[:index] = "You've been here #{session[:counter]} time".pluralize(session[:counter])+". Buy something or leave. This isn't a library."
-        render "index"
-        end
-      end
-      respond_to do |format|
-        format.html {
-          if (params[:spa] && params[:spa] == "true")
-              render 'index_spa'
-          # the else case below is by default
-          else
-             render 'index'
-          end
-          }
-        format.json {render json: @products}
+    
       end
     end
-  end
+    respond_to do |format|
+        format.html {
+            if (params[:spa] && params[:spa] == "true")
+                render 'index_spa'
+            # the else case below is by default
+            else
+               render 'index'
+            end
+        }
+        format.json {render json: @products}
+        end
+      end
+    end
