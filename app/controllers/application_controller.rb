8c1beb97308ab17acdea838a180e362862c67701
Sean Passmore <jeanghandi@gmail.com>
2019-02-05 13:50:10 -0500

    attempting to fix error in index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_global_cart
+
+ def set_global_cart
+   @global_cart = current_user.cart || current_user.cart.create()
+   //something similar & it will be better to use different name instead of @cart
+ end
   protect_from_forgery with: :exception
 end
