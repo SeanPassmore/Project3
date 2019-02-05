f34033a1b0735b2ffb6407e9dc0b50c4caaf4e11
Sean Passmore <jeanghandi@gmail.com>
2019-02-05 17:13:09 -0500

    fixed a counting bug

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,21 +2,20 @@ class StoreController < ApplicationController
 
   def index
     @products = Product.order(:popularity).reverse_order
-    if session[:counter].nil?
-      session[:counter] = 0
-    else
-      session[:counter] += 1
-      if session[:counter] > 5
-        flash.now[:index] = "You've been here #{session[:counter]} time".pluralize(session[:counter])+". Buy something or leave. This isn't a library."
-
-      end
-    end
     respond_to do |format|
         format.html {
             if (params[:spa] && params[:spa] == "true")
                 render 'index_spa'
             # the else case below is by default
             else
+               if session[:counter].nil?
+                 session[:counter] = 0
+               else
+                 session[:counter] += 1
+                 if session[:counter] > 5
+                   flash.now[:index] = "You've been here #{session[:counter]} time".pluralize(session[:counter])+". Buy something or leave. This isn't a library."
+                 end
+               end
                render 'index'
             end
         }
